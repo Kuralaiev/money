9b0d11d7958808c4d46187a9b4de4a571328a05b
oscar <askar.kuralayev@gmail.com>
2023-10-31 14:32:47 +0600

    chore(project) : Cofigure simple chat example.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.info("Hello")
+import {Telegraf} from 'telegraf'
+
+
+const bot = new Telegraf ("6696646330:AAF470oU1NpTRa_g7F1AkSrpDj2h0D7yoJQ")
+
+
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('os3car'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+
